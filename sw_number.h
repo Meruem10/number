f794a7b91da3427f7e3e051610a9dd291475ab70
nivethan <nive.yoga@outlook.com>
2022-01-15 19:07:12 +0100

    Added macros with the maximum values of the smaller and larger datatypes

--- sw_number.h
+++ sw_number.h
@@ -1,10 +1,13 @@
 #ifndef SW_NUMBER_H
 #define SW_NUMBER_H
 
-
 // Typedefs
 typedef unsigned char uint8_t;
 typedef uint8_t small_number_type; // set the small number type
+
+// Macros
+#define SMALL_NUMBER_MAX_VALUE = (1 << sizeof(small_number_type)*8) - 1 // maximum value of smaller datatype
+#define LARGE_NUMBER_MAX_VALUE = 2*SMALL_NUMBER_MAX_VALUE // maximum value of larger datatype
 
 // Struct
 typedef struct  {
@@ -13,10 +16,11 @@ typedef struct  {
 } large_number;
 
 
-// Functions
+// Function declaration (i.e. prototypes)
 large_number add(large_number* num_1, large_number* num_2);
 large_number subtract(large_number* num_1, large_number* num_2);
 large_number multiply(large_number* num_1, large_number* num_2);
 large_number division(large_number* num_1, large_number* num_2);
+
 
 #endif
