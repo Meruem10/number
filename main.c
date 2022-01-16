a9dfd326e953a699e7d017a0e9df0976ca7dbc69
nivethan <nive.yoga@outlook.com>
2022-01-16 15:46:07 +0100

    The addition function works, if no overflow happens

--- main.c
+++ main.c
@@ -8,10 +8,28 @@ int main(void)
     printf("Maximum large number: %u\n", LARGE_NUMBER_MAX_VALUE);
 
     // Some test cases
-    unsigned int num = 300;
-    large_number num_struct = num_to_struct(300);
-    print_num_binary(&num_struct);
-    print_num_decimal(&num_struct);
+    unsigned int num_1 = 300;
+    large_number num_1_struct = num_to_struct(num_1);
+
+    unsigned int num_2 = 200;
+    large_number num_2_struct = num_to_struct(num_2);
+
+    large_number num_3_struct = add(&num_1_struct, &num_2_struct);
+
+    printf("\nNumber 1: \n");
+    print_num_binary(&num_1_struct);
+    print_num_decimal(&num_1_struct);
+    printf("\n");
+
+    printf("Number 2: \n");
+    print_num_binary(&num_2_struct);
+    print_num_decimal(&num_2_struct);
+    printf("\n");
+
+    printf("Number 1 + Number 2: \n");
+    print_num_binary(&num_3_struct);
+    print_num_decimal(&num_3_struct);
+    printf("\n");
 
 
     return 0;
--- sw_number.c
+++ sw_number.c
@@ -58,13 +58,10 @@ large_number num_to_struct(unsigned int num)
     memset(&num_struct.high, 0, SMALL_NUMBER_BITS);
     memset(&num_struct.low, 0, SMALL_NUMBER_BITS);
 
-    // print_bit_array(num_struct.high, SMALL_NUMBER_BITS);
-    // print_bit_array(num_struct.low, SMALL_NUMBER_BITS);
-
     // Decimal number into binary and then into struct
     for(char i = 2*SMALL_NUMBER_BITS - 1; i >= 0; i--)
     {
-        if(i >= SMALL_NUMBER_BITS)  // higher bits
+        if(i >= SMALL_NUMBER_BITS)                                              // higher bits
         {
             if((num / (1 << i) > 0) || ((num % (1 << i) == 0) && num > 0))
             {
@@ -77,7 +74,7 @@ large_number num_to_struct(unsigned int num)
             }
 
         }
-        else                        // lower bits
+        else                                                                    // lower bits
         {
             if((num / (1 << i) > 0) || ((num % (1 << i) == 0) && num > 0))
             {
@@ -98,119 +95,72 @@ large_number num_to_struct(unsigned int num)
     return num_struct;
 }
 
-
-
 // Mathematical operations
-large_number all_operation(large_number* num_1, large_number* num_2, int choice); // can re-use the code for all mathematical operations
-
-/*
 large_number add(large_number* num_1, large_number* num_2)
 {
-    // Array saving small_number_type as unisigned chars (i.e. index each byte)
-    uint8_t num_1_low_bytes[SMALL_NUMBER_BYTES];
-    uint8_t num_1_high_bytes[SMALL_NUMBER_BYTES];
-    memset(num_1_low_bytes, 0, SMALL_NUMBER_BYTES);
-    memset(num_1_high_bytes, 0, SMALL_NUMBER_BYTES);
-    
-    uint8_t num_2_low_bytes[SMALL_NUMBER_BYTES];
-    uint8_t num_2_high_bytes[SMALL_NUMBER_BYTES];
-    memset(num_2_low_bytes, 0, SMALL_NUMBER_BYTES);
-    memset(num_2_high_bytes, 0, SMALL_NUMBER_BYTES);
-
-    uint8_t num_3_low_bits[SMALL_NUMBER_BITS];
-    uint8_t num_3_high_bits[SMALL_NUMBER_BITS];
-    memset(num_3_low_bits, 0, SMALL_NUMBER_BITS);
-    memset(num_3_high_bits, 0, SMALL_NUMBER_BITS);
-
-    // Copy 
-    memcpy(num_1_low_bytes, &num_1->low, sizeof(small_number_type));
-    memcpy(num_1_high_bytes, &num_1->high, sizeof(small_number_type));
-    memcpy(num_2_low_bytes, &num_2->low, sizeof(small_number_type));
-    memcpy(num_2_high_bytes, &num_2->high, sizeof(small_number_type));
-
-    // Go through all Bits of lower half
-    uint8_t num_1_low_bit = 0;
-    uint8_t num_2_low_bit = 0;
-    uint8_t num_1_high_bit = 0;
-    uint8_t num_2_high_bit = 0;
+    // Initialization
+    large_number num_3;
+    memset(&num_3.low, 0, SMALL_NUMBER_BYTES);
+    memset(&num_3.high, 0, SMALL_NUMBER_BYTES);
 
     uint8_t carry = 0;
-    for(uint8_t i = 0; i < SMALL_NUMBER_BITS; i++)
+    for(uint8_t i = 0; i < 2*SMALL_NUMBER_BITS; i++)
     {
-        // Link: https://stackoverflow.com/questions/44978126/structure-for-an-array-of-bits-in-c
-        // First part gives me the value in the specific Byte
-        // Second part shifts a 1 to the specific Bit location and compares with the Byte to see if it is a 1
-        num_1_low_bit = ((num_1_low_bytes[i/8]) & (1 << (i%8))) != 0; 
-        num_2_low_bit = ((num_2_low_bytes[i/8]) & (1 << (i%8))) != 0;
-
-        // Addition
-        switch(num_1_low_bit + num_2_low_bit + carry)
+        if(i < SMALL_NUMBER_BITS) // lower bits
         {
-            case 0:
-                num_3_low_bits[i] = 0;
+            switch(num_1->low[i] + num_2->low[i] + carry)
+            {
+                case 0:
+                num_3.low[i] = 0;
                 carry = 0;
                 break;
             case 1:
-                num_3_low_bits[i] = 1;
-                carry = 1;
+                num_3.low[i] = 1;
+                carry = 0;
                 break;
             case 2:
-                num_3_low_bits[i] = 0;
+                num_3.low[i] = 0;
                 carry = 1;
                 break;
             case 3:
-                num_3_low_bits[i] = 1;
+                num_3.low[i] = 1;
                 carry = 1;
-                break;
+                break; 
+            }
         }
-    }
-
-    // Go through all Bits of higher half
-    for(uint8_t i = 0; i < SMALL_NUMBER_BITS; i++)
-    {
-        num_1_high_bit = ((num_1_high_bytes[i/8]) & (1 << (i%8))) != 0; // Link: https://stackoverflow.com/questions/44978126/structure-for-an-array-of-bits-in-c
-        num_2_high_bit = ((num_2_high_bytes[i/8]) & (1 << (i%8))) != 0; 
-
-        // Addition
-        switch(num_1_high_bit + num_2_high_bit + carry)
+        else // higher bits
         {
-            case 0:
-                num_3_high_bits[i] = 0;
+            switch(num_1->high[i - SMALL_NUMBER_BITS] + num_2->high[i - SMALL_NUMBER_BITS] + carry)
+            {
+                case 0:
+                num_3.high[i -  SMALL_NUMBER_BITS] = 0;
                 carry = 0;
                 break;
             case 1:
-                num_3_high_bits[i] = 1;
-                carry = 1;
+                num_3.high[i - SMALL_NUMBER_BITS] = 1;
+                carry = 0;
                 break;
             case 2:
-                num_3_high_bits[i] = 0;
+                num_3.high[i - SMALL_NUMBER_BITS] = 0;
                 carry = 1;
                 break;
             case 3:
-                num_3_high_bits[i] = 1;
+                num_3.high[i - SMALL_NUMBER_BITS] = 1;
                 carry = 1;
-                break;
+                break; 
+            }
         }
     }
-    
-    // Check for overflow
+
+    // Overflow
     if(carry == 1)
     {
-        printf("Overflow happens!");
-    }
-
-    // Convert Bit array to decimal number and return large_number struct
-    large_number num_3 = {.high = 0, .low = 0};
-    for(uint8_t i = 0; i < SMALL_NUMBER_BITS; i++)
-    {
-        // Bit*2^i
-        num_3.low += (1 << i)*num_3_low_bits[i]; 
-        num_3.high += (1 << i)*num_3_high_bits[i];
+        printf("There is an overflow!\n");
     }
 
     return num_3;
 }
-*/
+
 
 large_number subtract(large_number* num_1, large_number* num_2)
 {
